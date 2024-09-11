c59b56bbdc12eec011f4b26b35ff5e481e968555
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-09-11 08:10:06 +0300

    Initial Commit

---
+++ Researcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Researcher
+//
+//  Created by bodya on 11.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
